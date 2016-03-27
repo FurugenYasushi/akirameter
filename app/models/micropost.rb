65e6651ecd45abeffd2fb24b44f3f382d1fb8489
furugenyasushi <koujieeee8@gmail.com>
2016-03-27 07:05:45 +0000

    cho

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :twitter_user
   validates :user_id, presence: false
   validates :content, presence: true, length: { maximum: 1000 }
+  validates :age, presence: true
+  validates :seibetsu, presence: true
+  validates :genre, presence: true
 end
